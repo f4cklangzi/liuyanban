c2d4ed2eb98934f2204970987a66f0a339e5d913
langzi-php <f4ck_langzi@foxmail.com>
2016-02-29 13:45:22 +0800

    mysql-port-2

--- includes/conn.php
+++ includes/conn.php
@@ -6,7 +6,7 @@
 *===================================================
 **/
 //数据库连接
-define('DB_HOST', SAE_MYSQL_HOST_M:SAE_MYSQL_PORT);
+define('DB_HOST', SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT);
 define('DB_USER', SAE_MYSQL_USER );
 define('DB_PWD', SAE_MYSQL_PASS);
 define('DB_NAME',SAE_MYSQL_DB);
